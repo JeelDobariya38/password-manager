da8b060dd2a196b2c7e690bd838efd86ed65c392
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-08-10 22:18:52 +0530

    feat: Made a Home Activity

--- app/src/main/java/com/passwordmanager/MainActivity.java
+++ app/src/main/java/com/passwordmanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.passwordmanager;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +11,46 @@ import androidx.core.view.WindowCompat;
 import com.passwordmanager.utils.Permissions;
 
 public class MainActivity extends AppCompatActivity {
-
+  
+  private Button savepasswordbtn;
+  private Button loadpasswordbtn;
+  private Button quitbtn;
+  
   private Permissions permissionsHandle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    // Initalizing the view elememts
+    savepasswordbtn = (Button)findViewById(R.id.save_password_activity_button);
+    loadpasswordbtn = (Button)findViewById(R.id.load_password_activity_button);
+    quitbtn = (Button)findViewById(R.id.quit_button);
+    
+    // Add event onclick listener
+    savepasswordbtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent savepasswordintent = new Intent(MainActivity.this, SavePasswordActivity.class);
+            startActivity(savepasswordintent);
+        }
+    });
+    
+    loadpasswordbtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // not implemented yet
+            return;
+        }
+    });
+    
+    quitbtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finishAndRemoveTask();
+        }
+    });
     
     // Make window fullscreen
     WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
@@ -39,10 +73,5 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_LONG).show();
       }
     }
-  }
-  
-  public void onClickSavePasswordBtn(View view) {
-      Intent savepasswordintent = new Intent(this, SavePasswordActivity.class);
-      startActivity(savepasswordintent);
   }
 }
