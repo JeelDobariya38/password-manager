0eee7641947e7c7a85a0ac132e3f19038cc8e241
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-08-19 17:56:20 +0530

    feat: refactor message in about screen

--- app/src/main/java/com/passwordmanager/ui/AboutUsActivity.java
+++ app/src/main/java/com/passwordmanager/ui/AboutUsActivity.java
@@ -1,12 +1,10 @@
 package com.passwordmanager.ui;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
-import android.view.LayoutInflater;
-
-import com.passwordmanager.R;
 import com.passwordmanager.databinding.ActivityAboutUsBinding;
 // import com.passwordmanager.utils.Permissions;
 
@@ -16,26 +14,34 @@ public class AboutUsActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     ActivityAboutUsBinding binding = ActivityAboutUsBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
-  
+
     // Add event onclick listener
     addOnClickListenerOnButton(binding);
-    
+
     // Make window fullscreen
     WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
   }
-  
+
+  private void openBrowser(String url) {
+    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    startActivity(browserIntent);
+  }
+
   // Added all the onclick event listiners
   private void addOnClickListenerOnButton(ActivityAboutUsBinding binding) {
-    binding.viewSecurityGuidelinesBtn.setOnClickListener(v -> {
-	    // TODO: open security guidelines
-	});
-	
-	binding.viewChangeLogBtn.setOnClickListener(v -> {
-		// TODO: open changelog
-	});
-	
-	binding.viewLicenseBtn.setOnClickListener(v -> {
-        // TODO: open license
-    });
+    binding.viewSecurityGuidelinesBtn.setOnClickListener(
+        v -> {
+          // TODO: open security guidelines
+        });
+
+    binding.viewChangeLogBtn.setOnClickListener(
+        v -> {
+          openBrowser("https://github.com/JeelDobariya38/password-manager/blob/main/changelog.md");
+        });
+
+    binding.viewLicenseBtn.setOnClickListener(
+        v -> {
+          openBrowser("https://github.com/JeelDobariya38/password-manager/blob/main/LICENSE.txt");
+        });
   }
 }
