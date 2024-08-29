9cdbedcd3831e0d2235016f7331751ac634487a8
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-08-29 13:30:31 +0530

    chore: create a way to have dev build separate then release

--- installondevice.bat
+++ installondevice.bat
@@ -1,10 +1,20 @@
 @REM Build & install on Android phone
 
 @REM Force continuation after Gradle command by capturing the output
-call gradle build
-if ERRORLEVEL 1 (
-    echo Gradle build failed! Check build_output.txt for details.
-    exit /b 1
+if "%~1"=="prod" (
+    echo building a production build
+    call gradlew clean assembleProdRelease
+    if ERRORLEVEL 1 (
+        echo Gradle build failed! Check build_output.txt for details.
+        exit /b 1
+    )
+) else (
+    echo building a development build
+    call gradlew assembleDevDebug
+    if ERRORLEVEL 1 (
+        echo Gradle build failed! Check build_output.txt for details.
+        exit /b 1
+    )
 )
 
 @REM Start ADB server
@@ -23,7 +33,21 @@ if ERRORLEVEL 1 (
 )
 
 @REM Install the APK on the connected device
-adb install ./app/build/outputs/apk/release/app-universal-release.apk
+if "%~1"=="prod" (
+    echo Installing a production apk
+    adb install ./app/build/outputs/apk/prod/release/app-prod-universal-release.apk
+    if ERRORLEVEL 1 (
+        echo Gradle build failed! Check build_output.txt for details.
+        exit /b 1
+    )
+) else (
+    echo Installing a dev apk
+    adb install ./app/build/outputs/apk/dev/debug/app-dev-universal-debug.apk
+    if ERRORLEVEL 1 (
+        echo Gradle build failed! Check build_output.txt for details.
+        exit /b 1
+    )
+)
 if ERRORLEVEL 1 (
     echo APK installation failed! Exiting...
     adb kill-server
