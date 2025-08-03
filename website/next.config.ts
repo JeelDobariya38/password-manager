c611f19346d23af1c3c1cee15cbbd2b787f50700
Jeel Dobariya <dobariyaj34@gmail.com>
2025-08-03 12:07:03 +0530

    ci(fix): URL with GitHub pages

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  assetPrefix: isProd ? "/Passcodes/" : "",
+  basePath: isProd ? "/Passcodes" : "",
 };
 
 export default nextConfig;
