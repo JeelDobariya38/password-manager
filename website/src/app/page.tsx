4289b0177873b3a0d7765f4681b7c15898bfa2ce
Jeel Dobariya <dobariyaj34@gmail.com>
2025-08-03 11:38:00 +0530

    feat: add a announcement label

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -4,6 +4,33 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex flex-col sm:flex-row justify-evenly items-center my-4 bg-slate-900 text-pink-500 rounded p-7">
+          <div className="px-4">
+            <Image
+              className="dark:invert"
+              src="/passcodes.png"
+              alt="Passcodes logo"
+              width={200}
+              height={100}
+              priority
+            />
+          </div>
+          <div>
+            <div className="pb-6 text-4xl font-black">
+              <h1>🥳🥳 Coming Soon!! 🎉🎉</h1>
+            </div>
+            <div className="text-lg text-cyan-900">
+              <p>
+                Passcodes Website & App will be offically release on <b>17/8</b>
+                .
+              </p>
+              <p>
+                Passcodes App will be unoffically (only for special users)
+                release on 16/8.
+              </p>
+            </div>
+          </div>
+        </div>
         <Image
           className="dark:invert"
           src="/next.svg"
