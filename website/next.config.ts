2bbdb3038fa293319b740465a64aafa2d7f45347
Jeel Dobariya <dobariyaj34@gmail.com>
2025-08-04 22:12:44 +0530

    fix: problem with custom image loader

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    loader: "custom",
-    loaderFile: "src/libs/custom-image-loader.ts",
-  },
   basePath: isProd ? "/Passcodes" : "",
   assetPrefix: isProd ? "/Passcodes/" : "",
 };
