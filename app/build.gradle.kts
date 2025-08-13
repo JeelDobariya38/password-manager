2117afdb469394759b8975c37c9e4dab25b70a14
Jeel Dobariya <dobariyaj34@gmail.com>
2025-08-13 22:09:33 +0530

    chore: update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
             minSdk = 26
             targetSdk = 34
             versionCode = 1
-            versionName = "0.1.0-Alpha"
+            versionName = "1.0.0-Stable"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
