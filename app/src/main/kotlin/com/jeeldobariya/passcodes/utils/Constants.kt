d56c8833db07ef5785416d6006bfdbaafcf0af9b
Jeel Dobariya <dobariyaj34@gmail.com>
2025-08-15 20:11:23 +0530

    feat: add a telegram community button

--- app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constant {
     const val REPORT_BUG_URL = "https://github.com/JeelDobariya38/password-manager/issues/new?template=bug-report.md"
     const val RELEASE_NOTE_URL = "https://github.com/JeelDobariya38/Passcodes/blob/main/docs/release-notes.md"
     const val SECURITY_GUIDE_URL = "https://github.com/JeelDobariya38/Passcodes/blob/main/docs/security-guide.md"
+    const val TELEGRAM_COMMUNITY_URL = "https://t.me/passcodescommunity"
 }
