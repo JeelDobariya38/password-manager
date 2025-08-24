0fa3f2eb1f693c1bb35517a53315e8dc59ab66ef
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-08-24 18:48:37 +0530

    refactor: pref name constants

--- app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
@@ -1,9 +1,18 @@
 package com.jeeldobariya.passcodes.utils
 
 object Constant {
+    // Url Constants
     const val REPO_URL = "https://github.com/JeelDobariya38/Passcodes"
     const val REPORT_BUG_URL = "https://github.com/JeelDobariya38/password-manager/issues/new?template=bug-report.md"
     const val RELEASE_NOTE_URL = "https://github.com/JeelDobariya38/Passcodes/blob/main/docs/release-notes.md"
     const val SECURITY_GUIDE_URL = "https://github.com/JeelDobariya38/Passcodes/blob/main/docs/security-guide.md"
     const val TELEGRAM_COMMUNITY_URL = "https://t.me/passcodescommunity"
+
+
+    // Shared Preferences Constants
+    const val APP_PREFS_NAME = "app_prefs"
+    const val THEME_KEY = "selected_theme"
+
+    const val FEATURE_FLAGS_PREFS_NAME = "feature_flags_prefs"
+    const val LATEST_FEATURES_KEY = "latest_features_enabled"
 }
